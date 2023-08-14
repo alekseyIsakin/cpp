d41a8a9d9158dba24c868c310a4c4a4ff4e71b79
alekseyIsakin <karahunforever@gmail.com>
2023-08-14 08:09:23 +0300

    client, handle server disconnect

--- app2/source/src/socket.cxx
+++ app2/source/src/socket.cxx
@@ -4,6 +4,16 @@
 #include <chrono>
 #include <ctime>
 
+int to_value(uint8_t *buffer, size_t len)
+{
+    int sum = 0;
+    for (size_t i = 0; i < sizeof(uint); i++)
+    {
+        sum += (buffer[i] << (8 * i));
+        std::clog << (int)buffer[i] << ' ' << (buffer[i] << (8 * i)) << '\n';
+    }
+    return sum;
+}
 void AppCommunicationClient::open_socket()
 {
     serverSocket = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -13,27 +23,40 @@ void AppCommunicationClient::open_socket()
     strcpy(serverAddress.sun_path, socketPath);
 
     int connection = -1;
-    while (connection == -1)
+    uint8_t buffer[sizeof(uint)];
+
+    while (true)
     {
         connection = connect(
             serverSocket,
             (struct sockaddr *)&serverAddress,
             sizeof(serverAddress));
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        perror("connect status");
-    }
 
-    while (true)
-    {
-        uint8_t buffer[1024];
-        int valread = read(serverSocket, buffer, 1024);
-
-        int i = 0;
-
-        std::cout << "[" << std::time(0) << "]" << valread << std::endl;
+        if (connection != 0)
+            perror("connection failed");
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
+        if (connection < 0)
+            continue;
+        std::clog << "connected" << std::endl;
+        int valread = -1;
 
+        do
+        {
+            valread = read(serverSocket, buffer, sizeof(uint));
+            if (valread == 0)
+                break;
+
+            std::clog << "[" << std::time(0) << "]" << valread << std::endl;
+            std::cout << to_value(buffer, valread) << std::endl;
+        } while (valread > 0);
+
+        // recreate socket when server is disconnect
+        if (valread == 0)
+        {
+            perror("disconnect: ");
+            serverSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+        }
     }
 }
 
